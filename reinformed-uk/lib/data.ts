39f7bff7bfacdfd24e801fb905cda876901017ba
TenebrisUKData <tenebris.solutions.uk@gmail.com>
2025-05-14 19:24:46 +0100

    Delete reinformed-uk directory

--- reinformed-uk/lib/data.ts
+++
@@ -1,6 +0,0 @@
-export const parties = [
-  { name: "Labour", color: "#E4003B", icon: "/assets/party-icons/labour.svg" },
-  { name: "Conservative", color: "#0087DC", icon: "/assets/party-icons/conservative.svg" },
-  { name: "Liberal Democrats", color: "#FDBB30", icon: "/assets/party-icons/libdem.svg" },
-  { name: "Green", color: "#6AB023", icon: "/assets/party-icons/green.svg" },
-];
--- reinformed-uk/lib/gsap.ts
+++
@@ -1,5 +0,0 @@
-import { gsap } from "gsap";
-
-export function fadeIn(selector: string) {
-  gsap.fromTo(selector, { opacity: 0 }, { opacity: 1, duration: 1 });
-}
